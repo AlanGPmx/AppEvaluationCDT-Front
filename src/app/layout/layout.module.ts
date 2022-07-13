dedf3fb9588bfe5ae945d3ca7f6feb4b499d2d4d
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-13 18:50:07 -0500

    fix: Pagina principal ya lista los productos

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material.module';
+import { LayoutComponent } from './layout.component';
+import { HeaderModule } from 'src/app/shared/components/header/header.module';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { ProductsModule } from '../pages/products/products.module';
+
+@NgModule({
+  declarations: [LayoutComponent],
+  imports: [
+    MaterialModule,
+    CommonModule,
+    HeaderModule,
+    MdbCarouselModule,
+    ProductsModule
+  ],
+  exports: [LayoutComponent]
+})
+export class LayoutModule { }
