90132d379df51bbb7c4ad729e83c1c93d192dd35
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-13 18:50:53 -0500

    feat: Interface de un producto

--- src/app/pages/products/interface/product.interface.ts
+++ src/app/pages/products/interface/product.interface.ts
@@ -2,7 +2,11 @@ export interface Product {
   id: number;
   name: string;
   price: number;
+  withDiscount: number;
   description: string;
   categoryId: number;
   stock: number;
+  img: string;
+  options: string[];
+  selected?: string;
 }
