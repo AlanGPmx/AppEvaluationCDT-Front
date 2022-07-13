205c824dfd780c9964cbc13f6bb3580cb92ff1cb
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-13 18:48:45 -0500

    fix: Nuevos elementos

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,10 +4,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import {MatBadgeModule} from '@angular/material/badge';
+import {MatSelectModule} from '@angular/material/select';
 
 
 @NgModule({
-  exports: [ MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule, MatBadgeModule ]
+  exports: [ MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule, MatBadgeModule, MatSelectModule ]
 })
 
 export class MaterialModule {}
