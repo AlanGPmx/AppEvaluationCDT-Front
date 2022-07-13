89fd5c0ced64b4371867b8a21bcf9b8c3309b0ea
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-13 18:51:41 -0500

    feat: Maquetado de un solo producto en card

--- src/app/pages/products/product/product.component.ts
+++ src/app/pages/products/product/product.component.ts
@@ -1,23 +1,45 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Product } from '../interface/product.interface';
+import { ShoppingCartService } from '../../../shared/services/shopping-cart.service';
 
 @Component({
+  providers: [ShoppingCartService],
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
-  @Input() product!: Product
+  @Input() product!: Product;
   @Output() addToCartClick = new EventEmitter<Product>();
+  buttonDisabled!: boolean;
+  showLinkMore!: boolean;
 
-  constructor() { }
+  constructor(
+    private shoppingCartService: ShoppingCartService,
+  ) {}
 
   ngOnInit(): void {
+    this.buttonDisabled = true;
   }
 
   addToCart(): void {
-    this.addToCartClick.emit(this.product)
+    this.addToCartClick.emit(this.product);
   }
 
+  changeOption(value: string): void {
+    this.product.selected = value;
+  }
+
+  allowAddToCart(): void {
+    this.buttonDisabled = false;
+  }
+
+  /**
+   * If the type of the value is a number, return true, otherwise return false.
+   * @param {any} val - The value to check.
+   * @returns A boolean value.
+   */
+  isNumber(val: any): boolean {
+    return this.shoppingCartService.isNumber(val);
+  }
 }
---
+++ src/app/pages/products/product/product.module.ts
@@ -0,0 +1,11 @@
+/* import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from 'src/app/material.module';
+
+@NgModule({
+  declarations: [ProductModule],
+  imports: [MaterialModule, CommonModule],
+  exports: [ProductModule],
+})
+export class ProductModule {}
+ */
