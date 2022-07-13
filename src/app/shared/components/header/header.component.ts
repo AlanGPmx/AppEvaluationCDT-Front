fd52282b29771959ab6fe18511b8c65cf2c57b56
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-13 18:53:27 -0500

    fix: Boton de carrito

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,20 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from '../../services/shopping-cart.service';
-
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html', // Se puede usar el TempleateInline pero se deja por defecto aunque no haya muchas lineas escritas
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor(private ShoppingCartService: ShoppingCartService) { }
+  constructor(
+    private ShoppingCartService: ShoppingCartService
+  ) {}
   quantity$ = this.ShoppingCartService.quantityAction$;
   total$ = this.ShoppingCartService.totalAction$;
   cart$ = this.ShoppingCartService.cartSAction$;
 
   ngOnInit(): void {
   }
-
 }
