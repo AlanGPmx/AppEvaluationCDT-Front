bdcc0b5e7caa27e02d63914f9b52e3d3d4543526
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-13 18:48:17 -0500

    feat: ruta para un elemento por su id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { SingleViewComponent } from './pages/products/single-view/single-view.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
   {path: '', component: LayoutComponent},
+  { path: 'product/:id', component: SingleViewComponent },
   { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
   { path: '**', component: PageNotFoundComponent }  // Dejar al final
 ];
