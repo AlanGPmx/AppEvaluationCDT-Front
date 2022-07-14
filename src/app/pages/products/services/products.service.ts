d575fe93961531fb394cda85f8386c4a95126381
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-14 10:38:30 -0500

    feat: BestSellers & getProduct By Id

--- src/app/pages/products/services/products.service.ts
+++ src/app/pages/products/services/products.service.ts
@@ -11,9 +11,20 @@ export class ProductsService {
   constructor(private http:HttpClient) { }
   private apiURL = 'http://localhost:3000/';
 
+  /* Getting the products from the API. */
   getProducts(): Observable<any> {
     return this.http.get<Product[]>(this.apiURL+'products');
   }
+
+  /* Getting the product from the API with the ID. */
+  getProduct(id:number): Observable<any> {
+    return this.http.get<Product[]>(this.apiURL+'products/'+id);
+  }
+
+/* Getting the product from the API with the ID. */
+  getBestSellers(): Observable<any> {
+    return this.http.get<Product[]>(this.apiURL+'bestSellers');
+}
 
 }
 
