ed3908d3ed283a0c5676d9bb9986b0901380457c
Alan Garduño P <alan.garduno@elektra.com.mx>
2022-07-15 16:50:23 -0500

    fix: Organizacion de codigo

--- src/app/pages/products/services/products.service.ts
+++ src/app/pages/products/services/products.service.ts
@@ -4,27 +4,24 @@ import { Observable } from 'rxjs';
 import { Product } from '../interface/product.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
-
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
   private apiURL = 'http://localhost:8000/api/v1/';
 
   /* Getting the products from the API. */
   getProducts(): Observable<any> {
-    return this.http.get<Product[]>(this.apiURL+'productos');
+    return this.http.get<Product[]>(this.apiURL + 'productos');
   }
 
   /* Getting the product from the API with the ID. */
-  getProduct(id:number): Observable<any> {
-    return this.http.get<Product[]>(this.apiURL+'productos/buscar?id='+id);
+  getProduct(id: number): Observable<any> {
+    return this.http.get<Product[]>(this.apiURL + 'productos/buscar?id=' + id);
   }
 
   /* Getting the best sellers from the API. */
   getBestSellers(): Observable<any> {
-    return this.http.get<Product[]>(this.apiURL+'productos/mas-vendidos');
+    return this.http.get<Product[]>(this.apiURL + 'productos/mas-vendidos');
+  }
 }
-
-}
-
